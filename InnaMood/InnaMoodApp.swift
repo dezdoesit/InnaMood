2977336013ffa1a1d9e2561ca8a7e4270aed1d2c
dezdoesit <dez.blair06@gmail.com>
2024-05-10 14:12:50 -0400

    Initial Commit

---
+++ InnaMood/InnaMoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnaMoodApp.swift
+//  InnaMood
+//
+//  Created by dmoney on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct InnaMoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
